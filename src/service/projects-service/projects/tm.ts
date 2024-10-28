ccd50a6519bbcc677fab6e83da0bf06f963c485f
a-rudak-EIS <arudak@ebsco.com>
2024-10-28 19:49:27 +0500

    fix: fix text

--- src/service/projects-service/projects/tm.ts
+++ src/service/projects-service/projects/tm.ts
@@ -29,7 +29,7 @@ export const TM: Project = {
       ],
     },
     {
-      id: "1",
+      id: "2",
       blockType: BLOCK_TYPE.imageColumnsContain,
       images: ["/tm/2.jpg"],
       text: [
@@ -37,7 +37,7 @@ export const TM: Project = {
       ],
     },
     {
-      id: "1",
+      id: "3",
       blockType: BLOCK_TYPE.halfImageRightContain,
       images: ["/tm/1.jpg"],
       text: [
@@ -45,7 +45,7 @@ export const TM: Project = {
       ],
     },
     {
-      id: "1",
+      id: "4",
       blockType: BLOCK_TYPE.imageColumnsContain,
       images: ["/tm/3.jpg"],
       text: [
@@ -53,39 +53,33 @@ export const TM: Project = {
       ],
     },
     {
-      id: "1",
+      id: "5",
       blockType: BLOCK_TYPE.textColumns,
       text: [
-        "Our team’s vision was to create an atmosphere that felt both open and intimate. We aimed to make each seat feel like a private corner within the lively energy of a bustling restaurant. To achieve this, we chose a mix of reclaimed materials, rich textures, and bold colors, tying it all together with warm, intentional lighting designed to enhance the cozy ambiance",
+        "We designed a contrasting palette of black metal with vibrant furniture in orange and teal, creating a visually stimulating space that still felt approachable. To add to the coziness, we focused on individualized lighting for each seating zone, crafting small pockets of warmth and privacy within the open layout",
         "By placing the bar at the heart of the layout, we established a focal point that greets patrons the moment they enter. This central bar serves as both a gathering point and a visual introduction to the pizzeria’s character, enhancing the overall flow and atmosphere",
       ],
     },
     {
-      id: "1",
+      id: "6",
       blockType: BLOCK_TYPE.imageColumns,
       images: ["/tm/7.jpg"],
-      text: [
-        "Our goal was clear: create an inviting, functional setting that maximized seating capacity without compromising comfort. We also wanted to position the bar as a central, welcoming feature that would draw guests in and set the tone for their experience",
-      ],
     },
     {
-      id: "1",
+      id: "7",
       blockType: BLOCK_TYPE.halfImageRightContain,
       images: ["/tm/5.jpg"],
       text: [
-        "Our goal was clear: create an inviting, functional setting that maximized seating capacity without compromising comfort. We also wanted to position the bar as a central, welcoming feature that would draw guests in and set the tone for their experience",
+        "Our team’s vision was to create an atmosphere that felt both open and intimate. We aimed to make each seat feel like a private corner within the lively energy of a bustling restaurant. To achieve this, we chose a mix of reclaimed materials, rich textures, and bold colors, tying it all together with warm, intentional lighting designed to enhance the cozy ambiance",
       ],
     },
     {
-      id: "1",
+      id: "8",
       blockType: BLOCK_TYPE.imageColumnsContain,
       images: ["/tm/6.jpg"],
-      text: [
-        "Our goal was clear: create an inviting, functional setting that maximized seating capacity without compromising comfort. We also wanted to position the bar as a central, welcoming feature that would draw guests in and set the tone for their experience",
-      ],
     },
     {
-      id: "1",
+      id: "9",
       blockType: BLOCK_TYPE.textColumns,
       text: [
         "The redesign exceeded expectations, leading to a 30% increase in sales post-renovation—a clear sign of customer satisfaction and comfort. According to the owner, the new design now attracts more patrons who are eager to stay longer and enjoy the ambiance. Knowing that our work had a direct, positive impact on the business was incredibly rewarding for the entire team",
