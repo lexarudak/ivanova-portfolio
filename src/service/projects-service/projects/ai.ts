e33454c4dbb5631c8ffc200c4eff7f868e1837d9
a-rudak-EIS <arudak@ebsco.com>
2024-11-01 15:34:52 +0500

    feat: add small text changes

--- src/service/projects-service/projects/ai.ts
+++ src/service/projects-service/projects/ai.ts
@@ -58,7 +58,7 @@ export const AI: Project = {
       id: "7",
       blockType: BLOCK_TYPE.textColumns,
       text: [
-        "From my perspective, this project was an exciting opportunity to work with a gigantic array of shapes. Designing the house felt like a creative playground, especially with the challenge of making it appear unreal-ly light despite its size. The use of console levers and contrasting colors added a unique dynamic, giving the house a modern, floating quality that plays with visual balance",
+        "From my perspective, this project was an exciting opportunity to work with a gigantic array of shapes. Designing the house felt like a creative playground, especially with the challenge of making it appear unreally light despite its size. The use of console levers and contrasting colors added a unique dynamic, giving the house a modern, floating quality that plays with visual balance",
       ],
     },
   ],
--- src/service/projects-service/projects/st.ts
+++ src/service/projects-service/projects/st.ts
@@ -24,7 +24,7 @@ export const ST = {
       images: ["/st/st4.webp"],
       text: [
         // eslint-disable-next-line quotes
-        'Light-Filled Corridors: We added transparent walls to previously dark corridors, allowing sunlight to filter in and eliminating the gloomy "dead ends." This also enables flexible zoning, where spaces can be opened or closed as needed.',
+        'Light-Filled Corridors: We added transparent walls to previously dark corridors, allowing sunlight to filter in and eliminating the gloomy "dead ends." This also enables flexible zoning, where spaces can be opened or closed as needed',
       ],
     },
     {
@@ -48,8 +48,8 @@ export const ST = {
       id: "5",
       blockType: BLOCK_TYPE.textColumns,
       text: [
-        "Open-Plan Living Room: We transformed the previously closed-off living space by tearing down walls and allowing light to flow throughout. The room now feels expansive, with seamless access to other areas.",
-        "Relocated Kitchen: The kitchen was moved to the far end of the apartment, creating a cozy, functional space that includes a large dining table perfect for hosting. The new kitchen features hidden storage, blending practicality with a clean design.",
+        "Open-Plan Living Room: We transformed the previously closed-off living space by tearing down walls and allowing light to flow throughout. The room now feels expansive, with seamless access to other areas",
+        "Relocated Kitchen: The kitchen was moved to the far end of the apartment, creating a cozy, functional space that includes a large dining table perfect for hosting. The new kitchen features hidden storage, blending practicality with a clean design",
       ],
     },
     {
@@ -67,7 +67,7 @@ export const ST = {
       id: "8",
       blockType: BLOCK_TYPE.textColumns,
       text: [
-        "Multi-Use Bedroom: The primary bedroom now includes a work area, ensuring flexibility. There’s also a separate office that serves as an additional workspace or guest room when needed.",
+        "Multi-Use Bedroom: The primary bedroom now includes a work area, ensuring flexibility. There’s also a separate office that serves as an additional workspace or guest room when needed",
       ],
     },
     {
@@ -75,7 +75,7 @@ export const ST = {
       blockType: BLOCK_TYPE.halfImageRight,
       images: ["/st/st7.webp"],
       text: [
-        "This apartment renovation creates a spacious, light-filled, and open environment, perfectly tailored for a couple with a focus on modern living and future flexibility.",
+        "This apartment renovation creates a spacious, light-filled, and open environment, perfectly tailored for a couple with a focus on modern living and future flexibility",
       ],
     },
 
@@ -84,7 +84,7 @@ export const ST = {
       blockType: BLOCK_TYPE.halfImage,
       images: ["/st/st10.webp"],
       text: [
-        "Enhanced Bathroom: We combined the original two bathrooms into a single, spacious one that includes a luxurious walk-in shower. A separate storage area houses the washing machine, keeping the bathroom clutter-free. ",
+        "Enhanced Bathroom: We combined the original two bathrooms into a single, spacious one that includes a luxurious walk-in shower. A separate storage area houses the washing machine, keeping the bathroom clutter-free",
       ],
     },
     {
@@ -96,8 +96,8 @@ export const ST = {
       id: "11",
       blockType: BLOCK_TYPE.textColumns,
       text: [
-        "To overcome the challenges of a dark, fragmented layout, we completely remained the apartment by removing walls and creating an open, light-filled living space. The new design maximizes natural light, functionality, and flexibility, making it ideal for a young couple, with future-proofing for family expansion.",
-        "The central cube with white glass trim was a key element in the design, serving both aesthetic and functional purposes. It functioned as a marker board, providing a space for the owner to jot down notes and read them at any time. Through this project, I was able to showcase my design skills and understanding of the balance between functionality and aesthetics in interior design.",
+        "To overcome the challenges of a dark, fragmented layout, we completely reimagined the apartment by removing walls and creating an open, light-filled living space. The new design maximizes natural light, functionality, and flexibility, making it ideal for a young couple, with future-proofing for family expansion",
+        "The central cube with white glass trim was a key element in the design, serving both aesthetic and functional purposes. It functioned as a marker board, providing a space for the owner to jot down notes and read them at any time. Through this project, I was able to showcase my design skills and understanding of the balance between functionality and aesthetics in interior design",
       ],
     },
   ],
